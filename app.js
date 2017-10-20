251891c0410b8d310e4b5e9a37d2fb1f9af612c2
rykcheung <32950929+rykcheung@users.noreply.github.com>
2017-10-20 15:38:14 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 app.get('/webhook', function (req, res) {
-    if (req.query['hub.verify_token'] === 'DQVJ1VWhZAUEM1aWhOWXZAWSXFfVm5aOHZAwcE5EQ0VfZA3JZAcFhaR3NxZAGhzSDkyWGU0dlJzbVc2V3VUeWV0TXF5eFZAkb2hpX1YwOGxvZAThIMk9ydDdaTThnMVZAyN3pldmJ1dG0zUGthNnpPRDlKS2VyYWxfajJlaS1td05GbFFTdDZAxdXBMalFIb0piWmJHT2FDdHRnTUZAOZAUJ6ZA21NZA2FoM2pzaUNWRGROdy0tYTNjTkhnR2dzbTNjLVpsQjBXUTlndnRkWHU3ZAk1YcDdzUEd5cQZDZD') {
+    if (req.query['hub.verify_token'] === 'TOKEN') {
       res.send(req.query['hub.challenge']);
     } else {
       res.send('Error, wrong validation token');    
