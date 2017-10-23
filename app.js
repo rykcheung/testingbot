05f2d4ba7553c9c9f2e6cbf238194cb6d064f189
rykcheung <raymond.yk.cheung@hkstp.org>
2017-10-23 11:12:50 +0800

    done

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-app.get('/webhook', function (req, res) {
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+
+});
+
+server.get('/webhook', function (req, res) {
     if (req.query['hub.verify_token'] === 'TOKEN') {
       res.send(req.query['hub.challenge']);
     } else {
